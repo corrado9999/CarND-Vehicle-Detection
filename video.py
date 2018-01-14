c889a4da28369d1acf71ebdbdf5063c9d1489ba6
Corrado Avolio <corrado9999@gmail.com>
2018-01-14 02:19:24 +0100

    Bugfixed subclip management.

--- video.py
+++ video.py
@@ -5,12 +5,12 @@ import click
 @click.command()
 @click.argument('input-file', type=click.Path())
 @click.argument('output-file')
-@click.option('--subclip', '-s', type=(float, float), default=None)
+@click.option('--subclip', '-s', type=(float, float), default=(None, None))
 @click.option('--codec', '-c', default=None)
 def main(input_file, output_file, subclip=None, codec=None):
     clip = VideoFileClip(input_file)
-    if subclip:
-        clip = clip.subclip(6, 10)
+    if subclip != (None, None):
+        clip = clip.subclip(*subclip)
     output = clip.fl_image(model.tracker)
     output.write_videofile(output_file, audio=False, codec=codec)
 
