c4b30f8182f7e9ac2e09f1bd8278ee0902c53a65
Corrado Avolio <corrado9999@gmail.com>
2018-01-15 01:21:41 +0100

    Improved model (especially on far cars) and added params for static processing.

--- model.py
+++ model.py
@@ -3,27 +3,33 @@ import training
 
 red = (79, 83, 217)
 blue = (202, 139, 66)
+green = (92, 184, 92)
 
-yranges = [(350, 680)]
-scales = [1]
-colors = [red]
-hog_cells_per_step = 4
-heat_threshold = 4
-fir_length = heat_threshold*2 - 1
+yranges = [(400, 500), (350, 580)]
+scales = [0.5, 1]
+colors = [blue, green, red] #one more for the merged boxes
+hog_cells_per_step = 2
+heat_thresholds = (32, 70) #75 60 (24, 60) (34, 55-60) (16, 50) (16, 46)
+fir_length = 12
 merge_boxes = True
-expand_maximum = True
 
-_, feature_extractor, classifier = training.load_model('./test.P')
+static_params = dict(
+    #yranges = [(400, 500), (350, 580), (500, 680)],
+    #scales = [0.5, 1, 2],
+    #colors = [blue, red, green],
+    #heat_thresholds = (3, 7),
+    heat_thresholds = (1, 3),
+)
+_, feature_extractor, classifier = training.load_model('./liblinear.P')
 
 tracker = tracking.CarTracker(yranges            = yranges,
                               scales             = scales,
                               colors             = colors,
                               hog_cells_per_step = hog_cells_per_step,
-                              heat_threshold     = heat_threshold,
+                              heat_thresholds    = heat_thresholds,
                               extract_features   = feature_extractor.extract_features,
                               predict            = classifier.predict,
                               fir_length         = fir_length,
                               merge_boxes        = merge_boxes,
-                              expand_maximum     = expand_maximum,
                              )
 
